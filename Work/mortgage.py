88df83f6b2694ed0aaebf04f848a3930ed324d63
Rob Jaret <141961778+rjjaret@users.noreply.github.com>
2025-07-04 13:21:59 -0400

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,36 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal=500000.0
+rate=.05
+payment=2684.11
+total_paid=0.0
+month=0
+extra_payment = 1000.0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+
+while principal > 0:
+	month=month+1
+	principal = principal * (1+rate/12) - payment
+	total_paid = total_paid + payment
+	
+	
+	if month >= extra_payment_start_month and month <= extra_payment_end_month:
+		principal = principal - extra_payment
+		total_paid = total_paid + extra_payment	
+		
+	if principal < 0:
+		overpayment = abs(principal)
+		total_paid - total_paid - overpayment
+		principal = 0
+	
+	print(month, round(total_paid, 2), round(principal, 2))
+# 	if month >= extra_payment_start_month and month <= extra_payment_end_month:
+# 		princpal = principal - extra_payment
+# 		total_paid = total_paid + extra_payment
+
+
+
+print('Total paid', round(total_paid, 2))
+print('Months', month)
