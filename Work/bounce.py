1c69f83ea145007cd54989df3fe59f5c15dc9459
Rob Jaret <141961778+rjjaret@users.noreply.github.com>
2025-07-04 13:20:58 -0400

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+height=100 #meters
+bounce_pct=3/5
+num_bounces=0
+
+while num_bounces <10:
+	num_bounces = num_bounces+1;
+	height=height*bounce_pct
+	print(num_bounces, round(height, 4))
+	
